6251b3a1e74996b8d3ce2626d8282ce22a56b281
TanDung <nguyentandung9111@gmail.com>
2023-08-10 00:00:36 +0700

    Init layout learn router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { NewsComponent } from './pages/news/news.component';
+// Mảng chứa các router
+const routes: Routes = [
+  // pathMatch: 'full' đã được thêm vào route mặc định để đảm bảo chỉ khớp khi URL hoàn toàn trùng khớp.
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'about', component: AboutComponent },
+  { path: 'news', component: NewsComponent },
+];
 
 @NgModule({
+  // Cái forRoot này quan trọng khởi tạo và cấu hình Router Module trong Angular, cho phép bạn quản lý việc điều hướng và hiển thị các thành phần khác nhau của ứng dụng dựa trên các route khác nhau.
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'LearnAllRouter';
+  // Vì link các đường link trên menu là đứng nên khai báo 1 mảng cho khỏe
+  listNav = [
+    { name: 'Home', link: '' },
+    { name: 'About', link: 'about' },
+    { name: 'News', link: 'news' },
+  ];
 }
